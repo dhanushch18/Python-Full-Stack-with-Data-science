cd592a7c5d627ba657e8775e34a8153dd2c705be
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-07-28 11:58:00 +0530

    Add files via upload

---
+++ bitwise oper.py
@@ -0,0 +1,8 @@
+
+print(6&3) #and#Sets each bit to 1 if both bits are 1
+print(8|2) #or #Sets each bit to 1 if one of two bits is 1
+print(8|2) #or #Sets each bit to 1 if only one of two bits is 1
+print(6^2) #xor Sets each bit to 1 if only one of two bits is 1
+print(~2) #not Inverts all the bits
+print(5<<3) #Zero fill left shift #Shift left by pushing zeros in from the right
+print(5>>3) #Signed right shift  #Shift right by pushing copies of the leftmost
