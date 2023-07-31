19a3b6df8bd4d6219c7da44c45112d4d7abfd8c7
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-07-31 11:42:13 +0530

    Add files via upload

---
+++ loops list.py
@@ -0,0 +1,22 @@
+#Print all items
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+  #Loop Through the Index Numbers
+
+l2 = ["abc", "bcd", "cherry"]
+for i in range(len(l2)):
+     print(l2[i])
+
+#Using a While Loop
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+  
+
