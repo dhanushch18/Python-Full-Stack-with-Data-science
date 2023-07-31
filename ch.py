8b7926a6ef0ec57f37db3c9a4c0d7549806f777b
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-07-31 11:16:49 +0530

    Add files via upload

---
+++ ch.py
@@ -0,0 +1,41 @@
+#arithmetic
+
+x=111
+y=11
+
+print("\n",(x+y),"\n",(x-y),"\n",(x*y),"\n",(x/y),"\n",(x^y),"\n",(x%y),"\n")
+
+#assignment
+
+x+=3
+y-=1
+print(x,y)
+
+#comparison
+
+print(x==y)
+
+print(x>y)
+
+#logical
+
+print(x>10 and x<9)
+print(x>10 or x<9)
+
+#identity
+
+print(x is y)
+print(x is not y)
+
+#membership
+
+c=["apple","banana"]
+print("apple" in c)
+
+#bitwise
+
+print ((6&4),(5|3),(4^3),(~4),(4<<2),(5>>3),)
+
+
+
+
---
+++ lists.py
@@ -0,0 +1,24 @@
+#Create a List
+
+list=["cricket","football","hockey"]
+print(list)
+
+#length of list
+
+print(len(list))
+
+#List items can be of any data type:
+
+l1=["vk","abd","cr"]
+l2=[18,17,7]
+l3=[True,False,True]
+print((l1),"\n",(l2),"\n",(l3),"\n",)
+
+#
+print(type(l2))
+
+#Using the list() constructor to make a List
+
+l = list (("vk","abd","cr"))
+print(l)
+
