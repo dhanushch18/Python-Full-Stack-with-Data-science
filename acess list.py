497e3fcbf02b56972bc79b5a3b25238095527993
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-07-31 11:16:31 +0530

    Add files via upload

---
+++ acess list.py
@@ -0,0 +1,22 @@
+l1 = ["vk","abd","cr","msd","yuvi"]
+print([l1],"\n",l1[1],"\n",l1[-1],"\n",l1[1:4])
+
+#Negative Indexing
+
+print([l1[-3]])
+
+#Range of Indexes
+
+print(l1[1:4])
+
+#from beginning
+
+print(l1[:4])
+
+#end
+
+print(l1[2:])
+
+#Range of Negative Indexes
+
+print(l1[-4:-1])
---
+++ changing.py
@@ -0,0 +1,20 @@
+#Change the second item:
+
+l1=["vk","abd","rcb","cr7","kbfc"]
+l1[3]="rr"
+print(l1)
+
+#Change a Range of Item Values
+
+l1[2:4]=["abc","xyz"]
+print(l1)
+
+#Change the second value by replacing it with two new values:
+
+l1[1:2]=["aaaa","bbbb"]
+print(l1)
+
+#Change the second and third value by replacing it with one value:
+
+l1[1:5]=["ch17"]
+print(l1)
