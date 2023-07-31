b2b0c916635b462f63566967199314e7fd791f9a
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-07-31 11:16:11 +0530

    Add files via upload

---
+++ insert.py
@@ -0,0 +1,12 @@
+#inserting items
+
+l1=["abc","bcd","xyz","vk18","rcb"]
+l1.insert(1,"ch17")
+print(l1)
+
+#extend list
+
+l2=["apple","mango"]
+l1.extend(l2)
+print(l1)
+
---
+++ remove files.py
@@ -0,0 +1,21 @@
+#remove items
+
+l1=["abc","bcd","xyz","vk18","rcb"]
+
+l1.remove("bcd")
+print(l1)
+
+#Remove Specified Index (.pop)
+
+l1.pop(1)
+print(l1)
+
+#delete full list
+
+del l1
+
+#Clear the List
+
+l2 = ["aaa","abbb","cddd","abcd"]
+l2.clear()
+print(l2)
