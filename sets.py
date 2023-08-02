36c23e589c7068121ea7c521dc65a545c085721f
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-02 10:46:16 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,13 @@
+set={"virat","msd","abd","ch"}
+print(set)
+
+set={"virat","msd","abd","ch",True,1,2}
+print(set)
+print(len(set))
+
+print(type(set))
+
+#
+s1= set(("virat","msd","abd","ch",True,2))
+
+print(s1)
