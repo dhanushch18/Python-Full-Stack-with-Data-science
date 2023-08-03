e70d59ececdd1cd9995ed002ae7c3277200f5b83
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-03 12:32:54 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+
+thisdict = {
+  "brand": "ROYAL ENFIELD",
+  "model": "CLASSIC 350",
+  "year": 1977
+}
+print(thisdict)
+
+#Print "brand" only
+
+print(thisdict["brand"])
+
+#data type
+
+print(type(thisdict))
+
+#length
+
+print(len(thisdict))
