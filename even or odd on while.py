9230894ae91e3c6bb177a5688bb106e5900b05fe
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-04 12:53:38 +0530

    Add files via upload

---
+++ even or odd on while.py
@@ -0,0 +1,9 @@
+#odd or even num
+
+i=int(input("enter a num :")) #for odd i=1 and even i=2
+while i<20:
+    print(i)
+    i+=2
+
+
+
---
+++ while loop.py
@@ -0,0 +1,10 @@
+#to print same variable
+i=1
+while i < 6:
+    print(i)
+
+    """
+i=2
+while i<7:
+    print(i)
+    i+=2"""
