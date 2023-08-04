6f2c70e0cd95360b8dea4999afacd01e1e171569
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-04 12:29:23 +0530

    Add files via upload

---
+++ input function.py
@@ -0,0 +1,11 @@
+username = input("enter username:")
+print("username is:" + username )
+
+userage = int(input("enter userage:"))
+print("userage is :{}".format(userage))
+
+num=float(input("num is:"))
+print("num is:{}".format(num))
+
+fav=input("fav sport is :")
+print("fav is :" + fav)
