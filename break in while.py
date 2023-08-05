b95f19b63c89d6b602b15c518e8efaf333f81273
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-05 13:11:28 +0530

    Add files via upload

---
+++ break in while.py
@@ -0,0 +1,9 @@
+#Exit the loop when i is 3:
+
+i=1
+
+while(1<9):
+    print(i)
+    if i == 7:
+      break
+    i+=1
---
+++ while loops.py
@@ -0,0 +1,8 @@
+#Print i as long as i is less than 6:
+
+i=0
+while i<6:
+    i += 1
+    print(i)
+
+ #to print from zero put this at end #  i+=1
