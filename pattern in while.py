d5efd8607afa8a22d62d91a1cc1ddbd79e26ac0f
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-05 13:10:30 +0530

    Add files via upload

---
+++ pattern in while.py
@@ -0,0 +1,8 @@
+i=1
+while i<=5:
+    j=1
+    while(j<=5):
+        print("a",end=" ")
+        j=j+1
+    print()
+    i=i+1
---
+++ pattern3 while.py
@@ -0,0 +1,16 @@
+#pyramid
+
+
+i=1
+while i<=5:
+    k=1
+    while(k<=k-i):
+       print(" ")
+    j=1
+    while(j<=i):
+        print("*",end=" ")
+        j=j+1
+    print()
+    i=i+1
+
+
