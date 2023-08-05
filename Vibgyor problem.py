641360c287d54043817553a5846bff0aa6b3a3b1
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-05 13:11:45 +0530

    Add files via upload

---
+++ Vibgyor problem.py
@@ -0,0 +1,18 @@
+#print vibgyor corresponding to each letter
+
+a=(input("Enter the variable:"))
+
+if(a=="V") or (a=="v"):
+    print("Violet " )
+elif(a=="I") or (a=="i"):
+    print("Indigo")
+elif(a=="B") or (a=="b"):
+    print("Blue")
+elif(a=="Y") or (a=="y"):
+    print("Yellow")
+elif(a=="O") or (a=="o"):
+    print("Orange")
+elif(a=="R") or (a=="r"):
+    print("Red")
+else:
+    print("Invalid")
---
+++ problem.py
@@ -0,0 +1,24 @@
+#print days according to number
+
+#a=1,2,3,4,5,6,7
+#b=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+
+
+a=int(input("enter the num :"))
+
+if(a==1):
+    print("Monday")
+elif(a==2):
+    print("Tuesday")
+elif(a==3):
+    print("Wednesday")
+elif(a==4):
+    print("Thursday")
+elif(a==5):
+    print("Friday")
+elif(a==6):
+    print("Saturday")
+elif(a==7):
+    print("Sunday")
+else:
+    print("Invalid")
