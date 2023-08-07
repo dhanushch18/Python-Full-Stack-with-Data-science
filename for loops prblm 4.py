e4c7e1c112ba0f70cfbe3ce357c925533ef89057
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-07 13:14:54 +0530

    Add files via upload

---
+++ for loops prblm 4.py
@@ -0,0 +1,8 @@
+
+for i in range (5):
+    k=1
+    for j in range (i+1):
+        print(k,end = " ")
+        k=i+1
+
+    print ()
---
+++ for loops prblm6.py
@@ -0,0 +1,6 @@
+for i in range (5):
+    k=0
+    for j in range (i+1):
+        print(k,end = " ")
+        k=k+1
+    print ()
