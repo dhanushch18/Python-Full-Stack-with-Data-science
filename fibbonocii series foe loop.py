39ab3a102329fef908443b38b5db5f2d787ee61c
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-08 13:06:13 +0530

    Add files via upload

---
+++ fibbonocii series foe loop.py
@@ -0,0 +1,18 @@
+#fibonocii series
+
+
+x=int(input("Enter the num:"))
+a=0
+b=1
+if(x==1):
+    print(" ",+a)
+else:
+    print("",+a)
+    print("",+b)
+for i in range (2,x):
+    c=a+b
+    a=b
+    b=c
+    print(" ",+c)
+
+
