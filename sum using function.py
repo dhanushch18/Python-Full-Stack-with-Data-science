e6419addc711fe4750ec4e828fc3fb4f3d267d67
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-09 13:11:03 +0530

    Add files via upload

---
+++ sum using function.py
@@ -0,0 +1,4 @@
+def sum(a,b):
+    sum1=a+b
+    return sum1
+print(sum(2,3))
---
+++ zero or + or - using function.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the num :"))
+
+def num(x):
+    if(x==0):
+        print("The num is zero")
+    elif (x>1):
+        print(f"The num {x} is  positive")
+    else:
+        print(f"The num {x} is negative")
+num(x)
