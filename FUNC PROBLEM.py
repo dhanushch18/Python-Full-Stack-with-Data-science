b97bfcd2380fc654c28960586a25501fb1b1aa16
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-10 12:49:12 +0530

    Add files via upload

---
+++ FUNC PROBLEM.py
@@ -0,0 +1,31 @@
+#x=int(input("Enter 2 num :"))
+#y=input()
+
+#add
+
+def func(x,y):
+    sum1=x+y
+    return sum1
+print(func(2,4))
+
+#substraction
+
+def func(a,b):
+
+    diff1=a-b
+    return diff1
+print(func(4,2))
+
+#multiplication
+
+def func(c,d):
+    mult1=c*d
+    return mult1
+print(func(3,2))
+
+#division
+
+def func(a,b):
+    div1=a/b
+    return div1
+print(func(6,3))
