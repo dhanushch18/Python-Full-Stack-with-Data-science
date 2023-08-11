8e32199fe8c1198a9ab0647200e3b7bfd0958691
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-11 10:17:43 +0530

    Add files via upload

---
+++ array problem.py
@@ -0,0 +1,33 @@
+num=[8,9,10]
+print(num)
+
+#set [1] to 17
+
+num[1]=17
+print(num)
+
+#add 4,5 & 6 to end of list
+#num=[8,17,10]
+num.append(4)
+num.append(5)
+num.append(6)
+print(num)
+
+#remove first variable
+num.remove(8)
+print(num)
+
+#sorting the list
+
+num.sort()
+print(num)
+
+#insert 25 at index 3
+
+num.insert(3,25)
+print(num)
+
+#double the list
+num=num*2
+
+print(num)
