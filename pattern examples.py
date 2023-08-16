220fbd3919c9924ac56be067c8800197de894285
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-16 10:01:09 +0530

    Add files via upload

---
+++ pattern examples.py
@@ -0,0 +1,33 @@
+i=1
+while i<=5:
+    j=1
+    while(j<=5):
+        print("a",end=" ")
+        j=j+1
+    print()
+    i=i+1
+
+
+i=1
+while i<=5:
+    j=1
+    while(j<=i):
+        print("*",end=" ")
+        j=j+1
+    print()
+    i=i+1
+
+
+
+i=1
+while i<=5:
+    k=1
+    while(k<=k-i):
+       print(" ")
+    j=1
+    while(j<=i):
+        print("*",end=" ")
+        j=j+1
+    print()
+    i=i+1
+
