c54d52ae021085ac998ce8a47ab5e6b6f2b8cde6
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-22 10:01:08 +0530

    Add files via upload

---
+++ pythonmath.py
@@ -0,0 +1,50 @@
+#Python Math
+
+#Python has a set of built-in math functions, including an extensive math module, that allows you to perform mathematical tasks on numbers.
+
+#Built-in Math Functions
+#The min() and max() functions can be used to find the lowest or highest value in an iterable:
+
+x=min(30,5,16)
+y=max(33,77,1)
+print(x)
+print(y)
+
+
+#The abs() function returns the absolute (positive) value of the specified number:
+
+x=abs(-7.25)
+print(x)
+
+#The pow(x, y) function returns the value of x to the power of y (xy).
+
+y=pow(2,4)
+print(y)
+
+#The Math Module
+
+#The math.sqrt() method for example, returns the square root of a number:
+
+import math
+
+z=math.sqrt(81)
+print(z)
+
+#The math.ceil() method rounds a number upwards to its nearest integer,
+# and the math.floor() method rounds a number downwards to its nearest integer, and returns the result:
+
+import math
+
+x=math.ceil(1.1)
+
+y=math.floor(1.4)
+print(x)
+print(y)
+
+#The math.pi constant, returns the value of PI (3.14...):
+import math
+
+x=math.pi
+
+print(x)
+
