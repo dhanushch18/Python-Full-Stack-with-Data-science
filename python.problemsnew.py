42472747189069da54a87ca7d700db09b58290d4
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-08-25 09:55:39 +0530

    Add files via upload

---
+++ python.problemsnew.py
@@ -0,0 +1,10 @@
+x=int(input("Enter a num :"))
+
+def pal(num):
+    x1 = num[::-1]
+    if x1 == x:
+        print('palindrome')
+    else:
+        print('not a palindrome')
+
+print(pal('edureka'))
